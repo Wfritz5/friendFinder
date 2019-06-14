7734cc0a24b6f156c4172f29d24b6744596dd72b
Will <wfritz5@gmail.com>
2019-06-14 11:52:59 -0600

    created friends.js array

---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+let friends = [{
+        name: "Will",
+        photo: "img.png",
+        scores: [5, 3, 4, 5, 4, 3, 2, 1, 2, 4]
+    },
+    {
+        name: "Natalie",
+        photo: "img.png",
+        scores: [5, 4, 3, 2, 5, 4, 2, 3, 5, 3]
+    },
+    {
+        name: "Bob",
+        photo: "img.png",
+        scores: [2, 3, 4, 5, 4, 3, 4, 3, 5, 2]
+    },
+    {
+        name: "Sammy",
+        photo: "img.png",
+        scores: [4, 3, 5, 3, 4, 5, 3, 4, 5, 4]
+    },
+    {
+        name: "Jim",
+        photo: "img.png",
+        scores: [2, 2, 2, 3, 3, 3, 4, 5, 4, 1]
+    },
+    {
+        name: "Jessica",
+        photo: "img.png",
+        scores: [4, 5, 3, 4, 3, 2, 5, 3, 2, 2]
+    },
+];
+
+module.exports = friends;
